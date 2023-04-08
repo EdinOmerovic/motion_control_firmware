e1b1071f5bace376c524cdc6335233cd5ba4c70a
Edin <edinomerovic2015@gmail.com>
2023-04-08 18:45:47 +0200

    Begore any integration

--- main.c
+++ main.c
@@ -3,24 +3,31 @@
 #include "F2837xD_device.h"        // F2837xD Headerfile Include File
 #include "F2837xD_Examples.h"      // F2837xD Examples Include File
 
+// GPIO
+//#define BLINKY_LED_GPIO 31
 
-#define BLINKY_LED_GPIO 31
+// QEP:
+
+
+// Zadatak:
+// Ukljuci i probaj sve module koji su ti neophodni za rad
+
+// Ubaciti QEP modul
+
+
 
 void main(void)
 {
     InitSysCtrl();
-    InitGpio();
-    GPIO_SetupPinMux(BLINKY_LED_GPIO, GPIO_MUX_CPU1, 0);
-    GPIO_SetupPinOptions(BLINKY_LED_GPIO, GPIO_OUTPUT, GPIO_PUSHPULL);
-    while(1){
-        GPIO_WritePin(BLINKY_LED_GPIO, 1);
-        DELAY_US(1000*500);
-        // Turn off LED
 
-        //
-        GPIO_WritePin(BLINKY_LED_GPIO, 0);
-        //
-        DELAY_US(1000*5000);
-    }
+
+
+    // GPIO
+    //InitGpio();
+    //GPIO_SetupPinMux(BLINKY_LED_GPIO, GPIO_MUX_CPU1, 0);
+    //GPIO_SetupPinOptions(BLINKY_LED_GPIO, GPIO_OUTPUT, GPIO_PUSHPULL);
+    //GPIO_WritePin(BLINKY_LED_GPIO, 1);
+
+
 
 }
